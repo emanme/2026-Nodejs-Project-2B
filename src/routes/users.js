368ca7702e03b62e6cf3f31a9b6828c3dfe79288
jedlad <jeddladores@gmail.com>
2026-03-10 17:11:36 +0800

    Fix weak password validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,11 +6,19 @@ const { register, login, me } = require('../controllers/userController');
 
 const router = express.Router();
 
+const passwordSchema = z
+  .string()
+  .min(8, "Password must be at least 8 characters long")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Password must contain at least one number")
+  .regex(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character");
+
 const registerSchema = z.object({
   body: z.object({
     email: z.string().email(),
     name: z.string().min(2),
-    password: z.string().min(1) // ISSUE-0008 weak policy in release
+    password: passwordSchema
   })
 });
 
